4ac3ded3dc337b7fe5c1918e4637e7f3213202de
Alex Kessinger <akessinger@bepress.com>
2018-02-17 14:05:51 -0500

    Depend on interface over struct

--- marcdex/marcdex.go
+++ marcdex/marcdex.go
@@ -2,6 +2,7 @@ package marcdex
 
 import (
 	"github.com/boltdb/bolt"
+	"github.com/boutros/marc"
 )
 
 // SubjectHeadingMarc is bilibographic record that gets indexed by id
@@ -13,16 +14,24 @@ type SubjectHeadingMarc struct {
 // MarcIndexer manages the marc index
 type MarcIndexer struct {
 	db *bolt.DB
+	ms IMarcStream
+}
+
+type IMarcStream interface {
+	Next() bool
+	Value() []*marc.Record
 }
 
 // MustNewMarcIndexer will return a new MarcIndexer or die
-func MustNewMarcIndexer(path string) *MarcIndexer {
+func MustNewMarcIndexer(path string, ms IMarcStream) *MarcIndexer {
 	db, err := bolt.Open(path, 0600, nil)
 	if err != nil {
 		panic(err)
 	}
+
 	m := &MarcIndexer{
 		db: db,
+		ms: ms,
 	}
 
 	return m
--- marcdex/marcdex_test.go
+++ marcdex/marcdex_test.go
@@ -4,9 +4,15 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/boutros/marc"
 	"github.com/stretchr/testify/assert"
 	"github.com/voidfiles/a/marcdex"
 )
+
+type TestMarcStream struct{}
+
+func (ms *TestMarcStream) Next() bool            { return true }
+func (ms *TestMarcStream) Value() []*marc.Record { return make([]*marc.Record, 0) }
 
 func TestMustNewMarcIndexer(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
@@ -14,10 +20,10 @@ func TestMustNewMarcIndexer(t *testing.T) {
 		t.Fail()
 	}
 	tmpPath := tmpDir + "/db"
-
-	marcdex.MustNewMarcIndexer(tmpPath)
+	ms := &TestMarcStream{}
+	marcdex.MustNewMarcIndexer(tmpPath, ms)
 	assert.Panics(t, func() {
-		marcdex.MustNewMarcIndexer("/asdf/sdfsd/sdfs/sdfs")
+		marcdex.MustNewMarcIndexer("/asdf/sdfsd/sdfs/sdfs", ms)
 	})
 
 }
